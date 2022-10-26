95b2238b61008766cb6ac6897e1cb03069b8cb30
Fabio Barbieri <fabio.barbieri777@outlook.com>
2022-10-26 17:36:42 -0300

    Playing video

---
+++ Pages/PlayVideo.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Web.Helpers;
+
+namespace AspNetCoreWebApp.Pages
+{
+    public class PlayVideoModel : PageModel
+    {
+    /*    private Video? _video;
+        [BindProperty]
+        public static Video Video;
+
+        public Task OnPostAsync()
+        {
+            if (_video != null)
+            {
+
+            }
+        }
+    */
+        public void OnGet()
+        {
+        }
+    }
+}
